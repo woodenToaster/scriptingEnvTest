51cfd86fdf5dcca8da5c21d7987fd667ad4bf233
woodenToaster <litmusik@yahoo.com>
2014-10-27 09:53:41 -0500

    add canvas

---
+++ public/javascripts/drawRect.js
@@ -0,0 +1,5 @@
+var c = document.getElementById('myCanvas');
+var ctx = c.getContext('2d');
+ctx.fillStyle = '#FF0000';
+ctx.fillRect(20, 20, 150, 100);
+ctx.stroke();
--- routes/createScript.js
+++ routes/createScript.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+var changes = {};
+
 // POST submit script
 router.post('/', function(req, res) {
-  var output = eval(req.body.scriptBody);
-  res.render('index', {scriptResult: output});
+  var tokens = req.body.scriptBody.split(' ');
+  changes[tokens[0]] = tokens[2];
+  res.render('index', {
+  	scriptResult: 'null',
+  	text: req.body.scriptBody
+  });
 });
 
 module.exports = router;

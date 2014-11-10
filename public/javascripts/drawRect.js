d451abc7deb49d8da210ddc73a7997f278c5833b
woodenToaster <litmusik@yahoo.com>
2014-11-09 20:01:05 -0600

    Embedded scripting working

--- public/javascripts/drawRect.js
+++ public/javascripts/drawRect.js
@@ -3,3 +3,16 @@ var ctx = c.getContext('2d');
 ctx.fillStyle = '#FF0000';
 ctx.fillRect(20, 20, 150, 100);
 ctx.stroke();
+$('#myCanvas').bind('click', function() {
+  var script = prompt("enter your script\n");
+  function update(data) {
+    
+    ctx.fillStyle = '#' + data;
+    ctx.fillRect(20, 20, 150, 100);
+    ctx.stroke();
+  };
+  $.ajax('/createScript/' + script, {
+    success: update,
+    type: 'POST'
+  });
+});
--- routes/createScript.js
+++ routes/createScript.js
@@ -4,13 +4,10 @@ var router = express.Router();
 var changes = {};
 
 // POST submit script
-router.post('/', function(req, res) {
-  var tokens = req.body.scriptBody.split(' ');
-  changes[tokens[0]] = tokens[2];
-  res.render('index', {
-  	scriptResult: 'null',
-  	text: req.body.scriptBody
-  });
+router.post('/:script', function(req, res) {
+  var text = req.params.script;
+  res.status(200);
+  res.send(text);
 });
 
 module.exports = router;
